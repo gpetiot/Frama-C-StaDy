937dee6269c8c6d6c5cc46a14ef0fb8bd0ce8d14
Guillaume Petiot <guillaume.petiot@cea.fr>
2013-12-16 16:22:03 +0100

    1st pass now fills tables terms_at_Pre and terms_at_stmt

--- register.ml
+++ register.ml
@@ -7,7 +7,8 @@ open Lexing
 
 type at_term =
 | Quantif_term of
-    term (* borne min *)
+    logic_var
+  * term (* borne min *)
   * term (* borne max *)
   * at_term
 | Unquantif_term of term
@@ -18,43 +19,18 @@ type at_term =
 
 let rec str_at_term = function
   | Unquantif_term t -> Pretty_utils.sfprintf "%a" Printer.pp_term t
-  | Quantif_term(t1,t2,a) ->
-    Pretty_utils.sfprintf "%s[%a,%a]"
-      (str_at_term a) Printer.pp_term t1 Printer.pp_term t2
+  | Quantif_term(v,t1,t2,a) ->
+    Pretty_utils.sfprintf "%s[%a,%a,%a]"
+      (str_at_term a)
+      Printer.pp_logic_var v
+      Printer.pp_term t1
+      Printer.pp_term t2
 
 let rec compareat x y = str_at_term x = str_at_term y
 
-let strlabel = function
-  | Label (str,_,_) -> str
-  | Case _ -> "case"
-  | Default _ -> "default"
-    
-let concat l = List.fold_left (fun x y -> x ^ y) "" l
-let strllabel = function
-  | StmtLabel {contents={labels=ll}} ->
-    concat (List.map strlabel ll)
-  | LogicLabel (_, str) -> str
 
 
 
-(*
-class c = object
-  inherit Visitor.frama_c_inplace
-
-  method! vpredicate _p =
-    match _p with
-    | Papp (li, labels, _) ->
-      begin
-	match li.l_body with
-	| LBpred _pred ->
-	  let cc = new c in
-	  let _ = cc#vpredicate _pred.content in
-	  DoChildrenPost (fun x -> x)
-	| _ -> DoChildrenPost (fun x -> x)
-      end
-    | _ -> DoChildrenPost (fun x -> x)
-end
-  *)
 
 
 class subst = object(self)
@@ -76,11 +52,11 @@ class subst = object(self)
 	  x, self#subst_term y labels args quantifs) new_args in
       begin
 	match li.l_body with
-	| LBnone -> failwith "LBnone unsupported"
-	| LBreads _ -> failwith "LBreads unsupported"
-	| LBterm _ -> failwith "LBterm unsupported"
+	| LBnone -> Options.Self.not_yet_implemented "LBnone"
+	| LBreads _ -> Options.Self.not_yet_implemented "LBreads"
+	| LBterm _ -> Options.Self.not_yet_implemented "LBterm"
 	| LBpred {content=p} -> self#subst_pred p new_labels new_args quantifs
-	| LBinductive _ -> failwith "LBinductive unsupported"
+	| LBinductive _ -> Options.Self.not_yet_implemented "LBinductive"
       end
     | Pseparated t -> Pseparated t
     | Prel (rel,t1,t2) -> Prel (rel,
@@ -102,13 +78,11 @@ class subst = object(self)
 			    self#subst_pnamed p2 labels args quantifs)
     | Plet (v,p) -> Plet (v, self#subst_pnamed p labels args quantifs)
     | Pforall (q,p) ->
-      let q' =
-	List.map (fun v -> {v with lv_name = "__quantif_" ^ v.lv_name}) q in
+      let q' = List.map (fun v -> {v with lv_name = "__q_" ^ v.lv_name}) q in
       let q'' = List.combine q q' in
       Pforall(q',self#subst_pnamed p labels args (List.rev_append q'' quantifs))
     | Pexists (q,p) ->
-      let q' =
-	List.map (fun v -> {v with lv_name = "__quantif_" ^ v.lv_name}) q in
+      let q' = List.map (fun v -> {v with lv_name = "__q_" ^ v.lv_name}) q in
       let q'' = List.combine q q' in
       Pexists (q,self#subst_pnamed p labels args (List.rev_append q'' quantifs))
     | Pat (p,l) -> Pat (self#subst_pnamed p labels args quantifs,
@@ -162,8 +136,8 @@ class subst = object(self)
 				  self#subst_term t1 labels args quantifs,
 				  self#subst_term t2 labels args quantifs)
     | TCastE (ty,t) -> TCastE (ty, self#subst_term t labels args quantifs)
-    | TAddrOf _ -> failwith "TAddrOf unsupported"
-    | TStartOf _ -> failwith "TStartOf unsupported"
+    | TAddrOf _ -> Options.Self.not_yet_implemented "TAddrOf"
+    | TStartOf _ -> Options.Self.not_yet_implemented "TStartOf"
     | Tapp (li,lassoc,params) ->
       let new_labels =
 	List.map (fun (x,y) -> x, self#subst_label y labels) lassoc in
@@ -173,14 +147,14 @@ class subst = object(self)
 	  x, self#subst_term y labels args quantifs) new_args in
       begin
 	match li.l_body with
-	| LBnone -> failwith "LBnone unsupported"
-	| LBreads _ -> failwith "LBreads unsupported"
+	| LBnone -> Options.Self.not_yet_implemented "LBnone"
+	| LBreads _ -> Options.Self.not_yet_implemented "LBreads"
 	| LBterm{term_node=t} -> self#subst_tnode t new_labels new_args quantifs
-	| LBpred _ -> failwith "LBpred unsupported"
-	| LBinductive _ -> failwith "LBinductive unsupported"
+	| LBpred _ -> Options.Self.not_yet_implemented "LBpred"
+	| LBinductive _ -> Options.Self.not_yet_implemented "LBinductive"
       end
     | Tlambda (q,t) -> Tlambda (q, self#subst_term t labels args quantifs)
-    | TDataCons _ -> failwith "constructor of logic sum-type unsupported"
+    | TDataCons _ -> Options.Self.not_yet_implemented "TDataCons"
     | Tif (t1,t2,t3) -> Tif (self#subst_term t1 labels args quantifs,
 			     self#subst_term t2 labels args quantifs,
 			     self#subst_term t3 labels args quantifs)
@@ -204,10 +178,8 @@ class subst = object(self)
     | Ttypeof t -> Ttypeof (self#subst_term t labels args quantifs)
     | Ttype t -> Ttype t
     | Tempty_set -> Tempty_set
-    | Tunion l -> Tunion (List.map
-			    (fun x -> self#subst_term x labels args quantifs) l)
-    | Tinter l -> Tinter (List.map
-			    (fun x -> self#subst_term x labels args quantifs) l)
+    | Tunion l->Tunion(List.map(fun x->self#subst_term x labels args quantifs)l)
+    | Tinter l->Tinter(List.map(fun x->self#subst_term x labels args quantifs)l)
     | Tcomprehension (t,q,None) ->
       Tcomprehension (self#subst_term t labels args quantifs, q, None)
     | Tcomprehension (t,q,Some p) ->
@@ -241,16 +213,211 @@ end
 
 
 
+class rm_at = object
+  inherit Visitor.frama_c_inplace
+
+  method! vterm term =
+    let f x = match x.term_node with Tat(t,_) -> t | _ -> x in
+    ChangeDoChildrenPost (term, f)
+end
+
+
+
+let result_involved term =
+  let ret = ref false in
+  let o = object
+    inherit Visitor.frama_c_inplace
+
+    method! vterm t =
+      match t.term_node with
+      | TLval (TResult _, _) -> ret := true; SkipChildren
+      | _ when !ret -> SkipChildren
+      | _ -> DoChildrenPost (fun x -> x)
+  end in
+  ignore (Visitor.visitFramacTerm o term);
+  !ret
+
+
+
+class find_bounds = object(self)
+  inherit Visitor.frama_c_inplace
+
+  val mutable in_quantif = false
+  val upper_bounds : term Cil_datatype.Logic_var.Hashtbl.t =
+    Cil_datatype.Logic_var.Hashtbl.create 32
+  val lower_bounds : term Cil_datatype.Logic_var.Hashtbl.t =
+    Cil_datatype.Logic_var.Hashtbl.create 32
+    
+  method! vpredicate pred =
+    match pred with
+    | Papp _ -> failwith "no application after substitution"
+    | Pat _ -> Options.Self.not_yet_implemented "Pat"
+    | Prel (Rle,{term_node=TLval(TVar v,TNoOffset)},t)
+    | Prel (Rge,t,{term_node=TLval(TVar v,TNoOffset)}) when in_quantif ->
+      if (result_involved t) then
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "\\result involved in %a, not added as upper bound of \\at-term"
+	  Printer.pp_term t
+      else
+	Cil_datatype.Logic_var.Hashtbl.add upper_bounds v t;
+      DoChildrenPost (fun x -> x)
+    | Prel (Rge,{term_node=TLval(TVar v,TNoOffset)},t)
+    | Prel (Rle,t,{term_node=TLval(TVar v,TNoOffset)}) when in_quantif ->
+      if (result_involved t) then
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "\\result involved in %a, not added as lower bound of \\at-term"
+	  Printer.pp_term t
+      else
+	Cil_datatype.Logic_var.Hashtbl.add lower_bounds v t;
+      DoChildrenPost (fun x -> x)
+    | Prel (Rlt,{term_node=TLval(TVar v,TNoOffset)},t)
+    | Prel (Rgt,t,{term_node=TLval(TVar v,TNoOffset)}) when in_quantif ->
+      if (result_involved t) then
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "\\result involved in %a, not added as upper bound of \\at-term"
+	  Printer.pp_term t
+      else
+	begin
+	  let exp_info = Cil.exp_info_of_term t in
+	  let tnode = TBinOp (MinusA, t, Cil.lone()) in
+	  let t' = Cil.term_of_exp_info t.term_loc tnode exp_info in
+	  Cil_datatype.Logic_var.Hashtbl.add upper_bounds v t'
+	end;
+      DoChildrenPost (fun x -> x)
+    | Prel (Rgt,{term_node=TLval(TVar v,TNoOffset)},t)
+    | Prel (Rlt,t,{term_node=TLval(TVar v,TNoOffset)}) when in_quantif ->
+      if (result_involved t) then
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "\\result involved in %a, not added as lower bound of \\at-term"
+	  Printer.pp_term t
+      else
+	begin
+	  let exp_info = Cil.exp_info_of_term t in
+	  let tnode = TBinOp (PlusA, t, Cil.lone()) in
+	  let t' = Cil.term_of_exp_info t.term_loc tnode exp_info in
+	  Cil_datatype.Logic_var.Hashtbl.add lower_bounds v t'
+	end;
+      DoChildrenPost (fun x -> x)
+    | Pforall (_,{content=Pimplies _})
+    | Pexists (_,{content=Pand _}) ->
+      in_quantif <- true;
+      DoChildrenPost (fun x -> in_quantif <- false; x)
+    | Pforall _ -> Options.Self.not_yet_implemented
+      "unsupported: try \\forall ... => ... instead"
+    | Pexists _ -> Options.Self.not_yet_implemented
+      "unsupported: try \\exists ... && ... instead"
+    | _ -> DoChildrenPost (fun x -> x)
+
+  val terms_at_Pre : (at_term list) Datatype.String.Hashtbl.t =
+    Datatype.String.Hashtbl.create 32
+  val terms_at_stmt : (at_term list) Cil_datatype.Stmt.Hashtbl.t =
+    Cil_datatype.Stmt.Hashtbl.create 32
+
+  method get_terms_at_Pre = terms_at_Pre
+  method get_terms_at_stmt = terms_at_stmt
+
+  method! vterm _t =
+    let find_or_abort hashtbl v =
+      try Cil_datatype.Logic_var.Hashtbl.find hashtbl v
+      with _ ->
+	Options.Self.abort "logic var %a unbounded" Printer.pp_logic_var v
+    in
+    let construct lv att =
+      let lower = find_or_abort lower_bounds lv in
+      let upper = find_or_abort upper_bounds lv in
+      let lower = Visitor.visitFramacTerm (new rm_at) lower in
+      let upper = Visitor.visitFramacTerm (new rm_at) upper in
+      (Quantif_term (lv, lower, upper, att))
+    in
+    let f x =
+      match x.term_node with
+      | Tat (t, StmtLabel stmt) ->
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "AT: \\at(%a,?) StmtLabel" Printer.pp_term t; 
+	let terms =
+	  try Cil_datatype.Stmt.Hashtbl.find terms_at_stmt !stmt
+	  with _ -> []
+	in
+	let lvars = Cil.extract_free_logicvars_from_term t in
+	let lvars = Cil_datatype.Logic_var.Set.filter
+	  (fun x -> x.lv_origin = None) lvars in
+	begin
+	  try
+	    let at_term =
+	      let t = Visitor.visitFramacTerm (new rm_at) t in
+	      Cil_datatype.Logic_var.Set.fold construct lvars (Unquantif_term t)
+	    in
+	    let terms = at_term :: terms in
+	    Cil_datatype.Stmt.Hashtbl.replace terms_at_stmt !stmt terms
+	  with
+	  | _ -> Options.Self.debug ~dkey:Options.dkey_first_pass
+	    "unbounded logic var: term %a not added in terms_at_stmt"
+	    Printer.pp_term t
+	end;
+	x
+      | Tat (t, LogicLabel(_stmtopt,label)) ->
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "AT: \\at(%a,%s) LogicLabel" Printer.pp_term t label;
+	begin
+	  try
+	    let func = (Extlib.the self#current_func).svar.vname in
+	    if label = "Pre" || label = "Old" then
+	      begin
+		let terms =
+		  try Datatype.String.Hashtbl.find terms_at_Pre func
+		  with _ -> []
+		in
+		let lvars = Cil.extract_free_logicvars_from_term t in
+		let lvars = Cil_datatype.Logic_var.Set.filter
+		  (fun x -> x.lv_origin = None) lvars in
+		begin
+		  try
+		    let at_term =
+		      let t' = Visitor.visitFramacTerm (new rm_at) t in
+		      Cil_datatype.Logic_var.Set.fold
+			construct lvars (Unquantif_term t')
+		    in
+		    let terms = at_term :: terms in
+		    Datatype.String.Hashtbl.replace terms_at_Pre func terms
+		  with
+		  | _ -> Options.Self.debug ~dkey:Options.dkey_first_pass
+		    "unbounded logic var: term %a not added in terms_at_Pre"
+		    Printer.pp_term t
+		end
+	      end
+	    else
+	      Options.Self.not_yet_implemented "AT: Tat (?, other label)"
+	  with _ -> ()
+	end;
+	x
+      | _ -> x
+    in
+    if result_involved _t then
+      begin
+	Options.Self.debug ~dkey:Options.dkey_first_pass
+	  "\\result involved in %a, \\at-term ignored"
+	  Printer.pp_term _t;
+	DoChildrenPost (fun x -> x)
+      end
+    else
+      DoChildrenPost f
+end
+
+
+
+
+
+
+
+
+
 let first_pass() =
   let dkey = Options.dkey_first_pass in
-  (* pour chaque fonction, les termes dont on veut la valeur en Pre *)
   let terms_at_Pre : (at_term list) Datatype.String.Hashtbl.t =
-    Datatype.String.Hashtbl.create 32
-  in
+    Datatype.String.Hashtbl.create 32 in
   let terms_at_stmt : (at_term list) Cil_datatype.Stmt.Hashtbl.t =
-    Cil_datatype.Stmt.Hashtbl.create 32
-  in
-  let quantif_bounds : (term * term) Stack.t = Stack.create() in
+    Cil_datatype.Stmt.Hashtbl.create 32 in
+  
   let o = object(self)
     inherit Visitor.frama_c_inplace
 
@@ -261,78 +428,58 @@ let first_pass() =
       | GFun(fundec,_) when Cil.is_unused_builtin fundec.svar -> SkipChildren
       | GVar(vi,_,_) when Cil.is_unused_builtin vi -> SkipChildren
       | GVarDecl(_,vi,_) when Cil.is_unused_builtin vi -> SkipChildren
+      | GAnnot _ -> SkipChildren
       | _ -> DoChildrenPost f
 
-    method! vterm _t =
-      let f x =
-	match x.term_node with
-	| Tat (t, StmtLabel stmt) ->
-	  Options.Self.debug ~dkey "\\at(%a,?) StmtLabel" Printer.pp_term t; 
-	  let terms =
-	    try Cil_datatype.Stmt.Hashtbl.find terms_at_stmt !stmt
-	    with _ -> []
+    method! vpredicate_named pred =
+      let c = new subst in
+      let p' = c#subst_pred pred.content [] [] [] in
+      Options.Self.debug ~dkey "avant: %a" Printer.pp_predicate pred.content;
+      Options.Self.debug ~dkey "après: %a" Printer.pp_predicate p';
+      Options.Self.debug ~dkey "===============================";
+      let fb = new find_bounds in
+      if self#current_func = None then
+	fb#reset_current_func()
+      else
+	fb#set_current_func  (Extlib.the self#current_func);
+      ignore (Visitor.visitFramacPredicate (fb :> Visitor.frama_c_inplace) p');
+      Datatype.String.Hashtbl.iter (fun k v ->
+	let t = try Datatype.String.Hashtbl.find terms_at_Pre k with _ -> [] in
+	let v' =
+	  let rec aux ret = function
+	    | [] -> ret
+	    | x::s-> aux(if List.exists(compareat x) ret then ret else x::ret) s
 	  in
-	  let terms = (Unquantif_term t) :: terms in
-	  Options.Self.debug ~dkey "add %a for stmt %a"
-	    Printer.pp_term t Printer.pp_stmt !stmt;
-	  Cil_datatype.Stmt.Hashtbl.replace terms_at_stmt !stmt terms;
-	  x
-	| Tat (t, LogicLabel(_stmtopt,label)) ->
-	  Options.Self.debug ~dkey "\\at(%a,%s) LogicLabel"
-	    Printer.pp_term t label;
-	  begin
-	    try
-	      let func = (Extlib.the self#current_func).svar.vname in
-	      if label = "Pre" || label = "Old" then
-		begin
-		  let terms =
-		    try Datatype.String.Hashtbl.find terms_at_Pre func
-		    with _ ->
-		      Options.Self.debug ~dkey "not found func %s" func; []
-		  in
-		  if List.exists (compareat (Unquantif_term t)) terms then
-		    ()
-		  else
-		    let terms = (Unquantif_term t) :: terms in
-		    Options.Self.debug ~dkey "add for func %s" func;
-		    Datatype.String.Hashtbl.replace terms_at_Pre func terms
-		end
-	      else
-		failwith "other label unsupported"
-	    with _ -> ()
-	  end;
-	  x
-	| _ -> x
-      in
-      DoChildrenPost f
-
-    method! vpredicate pred =
-      match pred with
-      | Pforall (q,p) -> ignore (p, q); DoChildrenPost (fun x -> x)
-      | Pexists (q,p) -> ignore (p, q); DoChildrenPost (fun x -> x)
-      | Papp (_li, _, _) ->
-	let c = new subst in
-	let p' = c#subst_pred pred [] [] [] in
-(*
-	Options.Self.debug ~dkey "avant: %a" Printer.pp_predicate pred;
-	begin
-	  match li.l_body with
-	  | LBterm t -> Options.Self.debug ~dkey "%a" Printer.pp_term t
-	  | LBpred p -> Options.Self.debug ~dkey "%a"
-                          Printer.pp_predicate_named p
-	  | _ -> assert false
-	end;
-	Options.Self.debug ~dkey "après: %a" Printer.pp_predicate p';
-*)
-	Options.Self.debug ~dkey "===============================";
-	ignore (self#vpredicate p');
-	DoChildrenPost (fun x -> x)
-      | _ -> DoChildrenPost (fun x -> x)
-
+	  aux [] v
+	in
+	let v' = List.filter (fun x -> not (List.exists (compareat x) t)) v' in
+	let new_terms = List.rev_append t v' in
+	Datatype.String.Hashtbl.replace terms_at_Pre k new_terms
+      ) fb#get_terms_at_Pre;
+      Cil_datatype.Stmt.Hashtbl.iter (fun k v ->
+	let t = try Cil_datatype.Stmt.Hashtbl.find terms_at_stmt k with _->[] in
+	let v' =
+	  let rec aux ret = function
+	    | [] -> ret
+	    | x::s-> aux(if List.exists(compareat x) ret then ret else x::ret) s
+	  in
+	  aux [] v
+	in
+	let v' = List.filter (fun x -> not (List.exists (compareat x) t)) v' in
+	let new_terms = List.rev_append t v' in
+	Cil_datatype.Stmt.Hashtbl.replace terms_at_stmt k new_terms
+      ) fb#get_terms_at_stmt;
+      SkipChildren
   end
   in
   Visitor.visitFramacFile o (Ast.get());
   terms_at_Pre, terms_at_stmt
+
+
+
+
+
+
 
 
 (* outputs the AST of a project in a file *)
@@ -473,8 +620,8 @@ let compute_props props =
   end;
   States.NbCases.mark_as_computed();
   States.TestFailures.mark_as_computed();
-  Options.Self.feedback "all-paths: %b" !Prop_id.all_paths;
-  Options.Self.feedback "%i test cases" (States.NbCases.get());
+  Options.Self.result "all-paths: %b" !Prop_id.all_paths;
+  Options.Self.result "%i test cases" (States.NbCases.get());
   let hyps = [] in
   let distinct = true in
   List.iter (fun prop ->
@@ -644,20 +791,20 @@ let run() =
       (*compute_props props;*)
       let terms_at_Pre, terms_at_stmt = first_pass() in
 
-      Options.Self.debug ~dkey:Options.dkey_first_pass "terms_at_Pre:";
+      Options.Self.debug ~dkey:Options.dkey_first_pass "R: terms_at_Pre:";
       Datatype.String.Hashtbl.iter_sorted (fun f terms ->
-	Options.Self.debug ~dkey:Options.dkey_first_pass "function '%s'" f;
+	Options.Self.debug ~dkey:Options.dkey_first_pass "R: function '%s'" f;
 	List.iter (fun x -> Options.Self.debug ~dkey:Options.dkey_first_pass
-	  "%s" (str_at_term x)) terms;
-	Options.Self.debug ~dkey:Options.dkey_first_pass "----------------"
+	  "R: %s" (str_at_term x)) terms;
+	Options.Self.debug ~dkey:Options.dkey_first_pass "R: ----------------"
       ) terms_at_Pre;
 
-      Options.Self.debug ~dkey:Options.dkey_first_pass "terms_at_stmt:";
+      Options.Self.debug ~dkey:Options.dkey_first_pass "R: terms_at_stmt:";
       Cil_datatype.Stmt.Hashtbl.iter_sorted (fun stmt terms ->
 	Options.Self.debug ~dkey:Options.dkey_first_pass
-	  "stmt %a" Printer.pp_stmt stmt;
+	  "R: stmt %a" Printer.pp_stmt stmt;
 	List.iter (fun x -> Options.Self.debug ~dkey:Options.dkey_first_pass
-	  "%s" (str_at_term x)) terms
+	  "R: %s" (str_at_term x)) terms
       ) terms_at_stmt;
 
       Datatype.String.Hashtbl.clear terms_at_Pre;
