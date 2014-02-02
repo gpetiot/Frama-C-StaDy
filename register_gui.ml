2e3a284d24b277099717da8166e1971d8119d27e
Guillaume Petiot <guillaume.petiot@cea.fr>
2014-02-02 15:37:09 +0100

    minor changes on register_gui

--- register_gui.ml
+++ register_gui.ml
@@ -11,16 +11,16 @@ let pc_panel (main_ui:Design.main_window_extension_points) =
   let packing = vbox#pack ~expand:true ~fill:true in
   let w = GPack.table ~packing ~columns:2 () in
   let box_4 = GPack.hbox ~packing:(w#attach ~left:1 ~top:4) () in
-  let tooltip =
-    Pretty_utils.sfprintf "%s" Kernel.MainFunction.parameter.Parameter.help in
+  let helper = Kernel.MainFunction.parameter.Parameter.help in
+  let tooltip = Pretty_utils.sfprintf "%s" helper in
   let validator s = List.mem s (Kernel.MainFunction.get_possible_values ()) in
-  let refresh =
-    Gtk_helper.on_string ~tooltip ~validator
-      box_4 "main" Kernel.MainFunction.get Kernel.MainFunction.set in
+  let get = Kernel.MainFunction.get in
+  let set = Kernel.MainFunction.set in
+  let refresh = Gtk_helper.on_string ~tooltip ~validator box_4 "main" get set in
   let run_button = GButton.button ~label:"Run" ~packing:(vbox#pack) () in
-  let _ = run_button#connect#pressed
-    (fun () -> main_ui#protect ~cancelable:true
-      (fun () -> Register.run(); main_ui#redisplay())) in
+  let callback() = Register.run(); main_ui#redisplay() in
+  let on_press() = main_ui#protect ~cancelable:true callback in
+  let _ = run_button#connect#pressed on_press in
   "pcva", vbox#coerce, Some refresh
   
 
@@ -36,27 +36,20 @@ let to_do_on_select
 	if button_nb = 1 then
 	  List.iter (fun (tc_c, l) ->
 	    main_ui#pretty_information "Counter-example (by PathCrawler-VA):@.";
-	    if tc_c <> "" then
-	      main_ui#pretty_information "%s@.@\n" tc_c;
-	    List.iter (fun (s,v) ->
-	      main_ui#pretty_information "%s = %s@." s v) l;
+	    if tc_c <> "" then main_ui#pretty_information "%s@.@\n" tc_c;
+	    List.iter(fun(s,v) -> main_ui#pretty_information "%s = %s@." s v) l;
 	    main_ui#pretty_information "------------------------@."
 	  ) testcases
 	else if button_nb = 3 then
 	  List.iter (fun(tc_c, _) ->
 	    let callback() =
 	      let prj = Project.create tc_c in
-	      Project.copy ~selection:
-		(State_selection.of_list[Kernel.PreprocessAnnot.self]) prj;
-	      Project.on prj (fun () ->
-		File.init_from_c_files [File.from_filename tc_c];
-		(*!Db.RteGen.compute()*)
-	      ) ()
+	      let sel = State_selection.of_list[Kernel.PreprocessAnnot.self] in
+	      Project.copy ~selection:sel prj;
+	      Project.on prj File.init_from_c_files [File.from_filename tc_c]
 	    in
-	    ignore
-	      (popup_factory#add_item
-		 (Printf.sprintf "_Open %s in new project" tc_c) ~callback
-	      )
+	    let item_str = Printf.sprintf "_Open %s in new project" tc_c in
+	    ignore (popup_factory#add_item item_str ~callback)
 	  ) testcases
       with
       | _ -> ()
@@ -79,5 +72,4 @@ let main main_ui =
   main_ui#register_panel pc_panel;
   main_ui#register_source_selector pc_selector
 
-let () =
-  Design.register_extension main
+let () = Design.register_extension main
