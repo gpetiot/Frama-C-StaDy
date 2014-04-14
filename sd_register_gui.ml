9c543ffd40df6c8508e0e164046b32093a6e2fa5
Guillaume Petiot <guillaume.petiot@cea.fr>
2014-04-14 09:42:45 +0200

    ugly fix for the GUI

--- sd_register_gui.ml
+++ sd_register_gui.ml
@@ -68,11 +68,14 @@ let pc_selector
 
 
 let main main_ui =
-  Sd_register.setup_props_bijection();
-  let lengths = Sd_register.lengths_from_requires() in
-  let terms_at_Pre = Sd_register.at_from_formals lengths in
-  let compute = (fun props -> Sd_register.compute_props props terms_at_Pre) in
-  main_ui#register_panel pc_panel;
-  main_ui#register_source_selector (pc_selector compute)
+  try
+    Sd_register.setup_props_bijection();
+    let lengths = Sd_register.lengths_from_requires() in
+    let terms_at_Pre = Sd_register.at_from_formals lengths in
+    let compute = (fun props -> Sd_register.compute_props props terms_at_Pre) in
+    main_ui#register_panel pc_panel;
+    main_ui#register_source_selector (pc_selector compute)
+  with _ -> ()
+
 
 let () = Design.register_extension main
