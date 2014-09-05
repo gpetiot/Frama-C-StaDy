bb8cfb77878aa811e2eb768f35f45551a547bc55
Guillaume Petiot <guillaume.petiot@cea.fr>
2014-09-05 11:09:10 +0200

    refactoring of sd_socket.ml

--- sd_socket.ml
+++ sd_socket.ml
@@ -1,158 +1,129 @@
 
 (* cut a string into 2 pieces *)
-let cut : string -> int -> string * string =
-  fun s n ->
-    try (String.sub s 0 n), (String.sub s n ((String.length s)-n))
-    with _ -> Sd_options.Self.abort "socket:cut \"%s\" %i" s n
+let cut s n =
+  try (String.sub s 0 n), (String.sub s n ((String.length s)-n))
+  with _ -> Sd_options.Self.abort "socket:cut \"%s\" %i" s n
 
-let process_test_case : string -> unit =
-  fun s ->
-    let cut_sep sep x =
-      let pos = String.index x sep in
-      let a,_ = cut x pos in
-      let _,b = cut x (pos+1) in
-      a,b
+let process_test_case s =
+  let cut_sep sep x =
+    let pos = String.index x sep in
+    let a,_ = cut x pos in
+    let _,b = cut x (pos+1) in
+    a,b
+  in
+  let str_tc, s = cut_sep '|' s in
+  let _msg, s = cut_sep ':' s in
+  let str_prop, s = try cut_sep '|' s with _ -> s, "" in
+  let id_prop = int_of_string str_prop in
+  let kind, s = try cut_sep '|' s with _ -> s, "" in
+  if kind <> "IN" && kind <> "OUTCONC" && kind <> "OUTSYMB" then
+    Sd_options.Self.abort "wrong value for kind: %s" kind;
+  let add_var_val acc str =
+    try let x, y = cut_sep '=' str in (x,y)::acc
+    with _ -> acc
+  in
+  let list_entries =
+    let rec aux acc str =
+      try let x,y = cut_sep '|' str in aux (add_var_val acc x) y
+      with _ -> add_var_val acc str
     in
-    let str_tc, s = cut_sep '|' s in
-    let _msg, s = cut_sep ':' s in
-    let str_prop, s = try cut_sep '|' s with _ -> s, "" in
-    let id_prop = int_of_string str_prop in
-    let kind, s = try cut_sep '|' s with _ -> s, "" in
-    if kind <> "IN" && kind <> "OUTCONC" && kind <> "OUTSYMB" then
-      (Sd_options.Self.debug
-	 ~dkey:Sd_options.dkey_socket "wrong value for kind: %s" kind;
-       assert false);
-    let add_var_val acc str =
-      try let x, y = cut_sep '=' str in (x,y)::acc
-      with _ -> acc
-    in
-    let list_entries =
-      let rec aux acc str =
-	try let x,y = cut_sep '|' str in aux (add_var_val acc x) y
-	with _ -> add_var_val acc str
-      in
-      if s = "" then [] else aux [] s
-    in
-    let prop = Sd_utils.to_prop id_prop in
-    let instru_file_name =
-      Printf.sprintf "__sd_instru_%s_%s.c"
+    if s = "" then [] else aux [] s
+  in
+  let prop = Sd_utils.to_prop id_prop in
+  let func = Kernel_function.get_name (fst (Globals.entry_point ())) in
+  let instru_file_name =
+    Printf.sprintf "__sd_instru_%s_%s.c"
       (Filename.chop_extension(Filename.basename(List.hd (Kernel.Files.get()))))
-      (Kernel_function.get_name (fst(Globals.entry_point())))
-    in
-    let func = Kernel_function.get_name (fst (Globals.entry_point ())) in
-    let f = "testcases_" ^ (Filename.chop_extension instru_file_name) in
-    let f = Filename.concat f func in
-    let f = Filename.concat f "testdrivers" in
-    let f = Filename.concat f ("TC_" ^ str_tc ^ ".c") in
-    try
-      let tbl = Sd_states.TestFailures.find prop in
-      try
-	let input,conc,symb = Datatype.String.Hashtbl.find tbl f in
-	let input, conc, symb =
-	  if kind = "IN" then list_entries, conc, symb
-	  else if kind = "OUTCONC" then input, list_entries, symb
-	  else input, conc, list_entries
-	in
-	Datatype.String.Hashtbl.replace tbl f (input,conc,symb);
-	Sd_states.TestFailures.replace prop tbl
-      with
-      | _ ->
-	let input, conc, symb =
-	  if kind = "IN" then list_entries, [], []
-	  else if kind = "OUTCONC" then [], list_entries, []
-	  else [], [], list_entries
-	in
-	Datatype.String.Hashtbl.add tbl f (input,conc,symb);
-	Sd_states.TestFailures.replace prop tbl
-    with
-    | _ ->
-      (* no counter-example for considered property *)
-      let new_tbl = Datatype.String.Hashtbl.create 32 in
-      let input, conc, symb =
-	if kind = "IN" then list_entries, [], []
-	else if kind = "OUTCONC" then [], list_entries, []
-	else [], [], list_entries
-      in
-      Datatype.String.Hashtbl.add new_tbl f (input,conc,symb);
-      Sd_states.TestFailures.add prop new_tbl
+      func
+  in
+  let f = "testcases_" ^ (Filename.chop_extension instru_file_name) in
+  let f = Filename.concat f func in
+  let f = Filename.concat f "testdrivers" in
+  let f = Filename.concat f ("TC_" ^ str_tc ^ ".c") in
+  let tbl =
+    try Sd_states.TestFailures.find prop
+    with Not_found -> Datatype.String.Hashtbl.create 32
+  in
+  let input, conc, symb =
+    try Datatype.String.Hashtbl.find tbl f
+    with Not_found -> [], [], []
+  in
+  let input, conc, symb =
+    if kind = "IN" then list_entries, conc, symb
+    else if kind = "OUTCONC" then input, list_entries, symb
+    else input, conc, list_entries
+  in
+  Datatype.String.Hashtbl.replace tbl f (input,conc,symb);
+  Sd_states.TestFailures.replace prop tbl
 
-let process_nb_test_cases : string -> unit =
-  fun s -> Sd_states.NbCases.set (int_of_string s)
 
-let process_final_status : unit -> unit =
-  fun () -> Sd_states.All_Paths.set true
+let process_nb_test_cases s = Sd_states.NbCases.set (int_of_string s)
+
+let process_final_status () = Sd_states.All_Paths.set true
     
-let process_reachable_stmt : string -> unit =
-  fun s ->
-    let sid = int_of_string s in
-    Sd_states.Unreachable_Stmts.remove sid
+let process_reachable_stmt s =
+  Sd_states.Unreachable_Stmts.remove (int_of_string s)
 
-let process_reachable_bhv : string-> unit =
-  fun s ->
-    let id = int_of_string s in
-    let kf,bhv,_ = Sd_states.Behavior_Reachability.find id in
-    Sd_states.Behavior_Reachability.replace id (kf,bhv,true)
+let process_reachable_bhv s =
+  let id = int_of_string s in
+  let kf,bhv,_ = Sd_states.Behavior_Reachability.find id in
+  Sd_states.Behavior_Reachability.replace id (kf,bhv,true)
 
 
 (* le mot-clé au début de la chaîne permet de savoir que faire des données
    en fin de chaîne, on se redirige vers la fonction de traitement
    correspondante *)
-let process_string : string -> unit =
-  fun s ->
-    try
-      let s1, s2 = cut s 3 in
-      if s1 = "TC|" then process_test_case s2
+let process_string s =
+  try
+    let s1, s2 = cut s 3 in
+    if s1 = "TC|" then process_test_case s2
+    else
+      let s1, s2 = cut s 5 in
+      if s1 = "NbTC|" then process_nb_test_cases s2
       else
-	let s1, s2 = cut s 5 in
-	if s1 = "NbTC|" then process_nb_test_cases s2
+	let s1, _s2 = cut s 14 in
+	if s1 = "FinalStatus|OK" then process_final_status ()
 	else
-	  let s1, _s2 = cut s 14 in
-	  if s1 = "FinalStatus|OK" then process_final_status ()
+	  let s1, s2 = cut s 14 in
+	  if s1 = "REACHABLE_BHV:" then process_reachable_bhv s2
 	  else
-	    let s1, s2 = cut s 14 in
-	    if s1 = "REACHABLE_BHV:" then process_reachable_bhv s2
-	    else
-	      let s1, s2 = cut s 15 in
-	      if s1 = "REACHABLE_STMT:" then process_reachable_stmt s2
-	      else assert false
-    with _ ->
-      Sd_options.Self.debug ~dkey:Sd_options.dkey_socket "'%s' not processed" s
+	    let s1, s2 = cut s 15 in
+	    if s1 = "REACHABLE_STMT:" then process_reachable_stmt s2
+	    else assert false
+  with _ ->
+    Sd_options.Self.debug ~dkey:Sd_options.dkey_socket "'%s' not processed" s
 
 
 (* filtre les chaînes de caractères reçues, on ne traite que celles qui
    sont préfixées par @FC: *)
-let rec process_channel : in_channel -> unit =
-  fun c ->
-    try
-      let str = input_line c in
-      begin
-	if str <> "" then
-	  let dkey = Sd_options.dkey_socket in
-	  Sd_options.Self.debug ~dkey "'%s' received" str;
-	  let prefix, suffix = cut str 4 in
-	  if prefix = "@FC:" then process_string suffix
-	  else Sd_options.Self.debug ~dkey "'%s' not processed" str
-      end;
-      process_channel c
-    with End_of_file -> ()
+let rec process_channel c =
+  try
+    let str = input_line c in
+    begin
+      if str <> "" then
+	let dkey = Sd_options.dkey_socket in
+	Sd_options.Self.debug ~dkey "'%s' received" str;
+	let prefix, suffix = cut str 4 in
+	if prefix = "@FC:" then process_string suffix
+	else Sd_options.Self.debug ~dkey "'%s' not processed" str
+    end;
+    process_channel c
+  with End_of_file -> ()
 
 
 (* récupère le cannal d'écoute de la socket *)
-let process_socket : Unix.file_descr -> unit =
-  fun s ->
-    let in_chan = Unix.in_channel_of_descr s in
-    process_channel in_chan;
-    close_in in_chan
+let process_socket s =
+  let in_chan = Unix.in_channel_of_descr s in
+  process_channel in_chan;
+  close_in in_chan
 
 
-
-let print_exit_code : Unix.process_status -> unit =
-  fun code ->
-    let str =
-      match code with
-      | Unix.WEXITED _ -> "OK"
-      | Unix.WSIGNALED _ ->  "killed"
-      | Unix.WSTOPPED _ -> "stopped"
-    in
-    Sd_options.Self.feedback ~dkey:Sd_options.dkey_socket "PathCrawler %s!" str
+let print_exit_code code =
+  let str =
+    match code with
+    | Unix.WEXITED _ -> "OK"
+    | Unix.WSIGNALED _ ->  "killed"
+    | Unix.WSTOPPED _ -> "stopped"
+  in
+  Sd_options.Self.feedback ~dkey:Sd_options.dkey_socket "PathCrawler %s!" str
     
