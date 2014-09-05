c55026a9dfc7276eba1107d19e52596b6ac38421
Guillaume Petiot <guillaume.petiot@cea.fr>
2014-09-05 15:15:09 +0200

    shortened sd_register

--- sd_register.ml
+++ sd_register.ml
@@ -2,16 +2,12 @@
 open Cil_types
 
 
-let typically_typer ~typing_context ~loc bhv ps =
-  match ps with
+let typically_typer ~typing_context ~loc bhv = function
   | p::[] ->
-    bhv.b_extended <-
-      ("typically",42,
-       [Logic_const.new_predicate 
-           (typing_context.Logic_typing.type_predicate 
-	      (typing_context.Logic_typing.pre_state) 
-	      p)])
-    ::bhv.b_extended
+    let f = typing_context.Logic_typing.type_predicate in
+    let state = typing_context.Logic_typing.pre_state in
+    let pred = Logic_const.new_predicate (f state p) in
+    bhv.b_extended <- ("typically", 42, [pred]) ::bhv.b_extended
   | _ -> typing_context.Logic_typing.error loc
     "expecting a predicate after keyword 'typically'"
 
@@ -20,23 +16,20 @@ let () = Logic_typing.register_behavior_extension "typically" typically_typer
 
 let second_pass filename props =
   Kernel.Unicode.set false;
-
   let gatherer = new Sd_insertions.gather_insertions props in
   Visitor.visitFramacFile (gatherer :> Visitor.frama_c_inplace) (Ast.get());
   let insertions = gatherer#get_insertions() in
-
-  Hashtbl.iter (fun k v ->
+  let print_insertions_at_label lab insertions =
     let dkey = Sd_options.dkey_insertions in
     let f ins =
       Sd_options.Self.feedback ~dkey "/* %a */ %a"
-	Sd_print.pp_label k Sd_print.pp_insertion_lb ins
+	Sd_print.pp_label lab Sd_print.pp_insertion_lb ins
     in
-    Queue.iter f v;
+    Queue.iter f insertions;
     Sd_options.Self.feedback ~dkey "--------------------"
-  ) insertions;
-
-  let printer =
-    new Sd_print.print_insertions insertions () in
+  in
+  Hashtbl.iter print_insertions_at_label insertions;
+  let printer = new Sd_print.print_insertions insertions () in
   let buf = Buffer.create 512 in
   let fmt = Format.formatter_of_buffer buf in
   printer#file fmt (Ast.get());
@@ -61,37 +54,31 @@ let emitter =
 
 
 let compute_props props =
-  let precond_file_name =
-    Printf.sprintf "__sd_%s_%s.pl"
-      (Filename.chop_extension(Filename.basename(List.hd (Kernel.Files.get()))))
-      (Kernel_function.get_name (fst(Globals.entry_point())))
-  in
-  let instru_file_name =
-    Printf.sprintf "__sd_instru_%s_%s.c"
-      (Filename.chop_extension(Filename.basename(List.hd (Kernel.Files.get()))))
-      (Kernel_function.get_name (fst(Globals.entry_point())))
-  in
-
+  let fname =
+    Filename.chop_extension(Filename.basename(List.hd (Kernel.Files.get()))) in
+  let kf = fst (Globals.entry_point()) in
+  let entry_point = Kernel_function.get_name kf in
+  let precond_fname = Printf.sprintf "__sd_%s_%s.pl" fname entry_point in
+  let instru_fname = Printf.sprintf "__sd_instru_%s_%s.c" fname entry_point in
   (* Translate some parts of the pre-condition in Prolog *)
   let native_precond_generated =
-    try Sd_native_precond.translate precond_file_name; true with _ -> false in
+    try Sd_native_precond.translate precond_fname; true with _ -> false in
   Sd_options.Self.feedback ~dkey:Sd_options.dkey_native_precond
     "Prolog pre-condition %s generated"
     (if native_precond_generated then "successfully" else "not");
-  let kf = fst (Globals.entry_point()) in
-  let translated_props =
-    second_pass instru_file_name props in
+  let translated_props = second_pass instru_fname props in
   let test_params =
     if native_precond_generated then
-      Printf.sprintf "-pc-test-params %s" precond_file_name
+      Printf.sprintf "-pc-test-params %s" precond_fname
     else
       ""
   in
   let cmd =
     Printf.sprintf
-      "frama-c -add-path /usr/local/lib/frama-c/plugins %s -main %s -pc -pc-gmp -pc-validate-asserts %s -pc-com %s -pc-no-xml %s"
-      instru_file_name
-      (Kernel_function.get_name (fst(Globals.entry_point())))
+      "frama-c -add-path /usr/local/lib/frama-c/plugins %s -main %s \
+ -pc -pc-gmp -pc-validate-asserts %s -pc-com %s -pc-no-xml %s"
+      instru_fname
+      entry_point
       test_params
       (Sd_options.Socket_Type.get())
       (Sd_options.PathCrawler_Options.get())
@@ -109,8 +96,7 @@ let compute_props props =
 	  Unix.listen socket 2;
 	  let ret = Unix.system cmd in
 	  let rec aux cpt =
-	    if cpt = 3 then ()
-	    else
+	    if cpt < 3 then
 	      let client, _ = Unix.accept socket in
 	      Sd_socket.process_socket client;
 	      Sd_socket.print_exit_code ret;
@@ -119,8 +105,7 @@ let compute_props props =
 	  aux 0
 	with _ ->
 	  Unix.close socket;
-	  Sd_options.Self.feedback ~dkey:Sd_options.dkey_socket
-	    "error: unix socket now closed!"
+	  Sd_options.Self.abort "unix socket now closed!"
       end;
       Unix.close socket;
       Sys.remove name
@@ -132,8 +117,7 @@ let compute_props props =
 	  Unix.listen socket 2;
 	  let ret = Unix.system cmd in
 	  let rec aux cpt =
-	    if cpt = 3 then ()
-	    else
+	    if cpt < 3 then
               let client, _ = Unix.accept socket in
 	      Sd_socket.process_socket client;
 	      Sd_socket.print_exit_code ret;
@@ -142,8 +126,7 @@ let compute_props props =
 	  aux 0
 	with _ ->
 	  Unix.close socket;
-	  Sd_options.Self.feedback ~dkey:Sd_options.dkey_socket
-	    "error: internet socket now closed!"
+	  Sd_options.Self.abort "internet socket now closed!"
       end;
       Unix.close socket
     | _ (* stdio *) ->
@@ -165,7 +148,7 @@ let compute_props props =
       List.map (Property.ip_of_requires kf Kglobal bhv) typically_preds
     with _ -> []
   in
-  List.iter (fun prop ->
+  let emit_status prop =
     try
       let _ = Sd_states.TestFailures.find prop in
       let status = Property_status.False_and_reachable in
@@ -176,20 +159,23 @@ let compute_props props =
       let hyps = strengthened_precond in
       if Sd_states.All_Paths.get() then
 	Property_status.emit emitter ~hyps prop ~distinct status
-  ) translated_props;
+  in
+  List.iter emit_status translated_props;
+  let add_assert_false sid (stmt, kf) =
+    Sd_options.Self.feedback "stmt %i unreachable" sid;
+    Annotations.add_assert ~kf emitter stmt Logic_const.pfalse
+  in
+  let info_reachability _ (kf,bhv,is_reachable) =
+    Sd_options.Self.feedback "behavior '%s' of function '%s' %s"
+      bhv.b_name
+      (Kernel_function.get_name kf)
+      (if is_reachable then "reachable" else "not reachable")
+  in
   if Sd_states.All_Paths.get() && strengthened_precond = [] then
     begin
-      Sd_states.Unreachable_Stmts.iter (fun sid (stmt, kf) ->
-	Sd_options.Self.feedback "stmt %i unreachable" sid;
-	Annotations.add_assert ~kf emitter stmt Logic_const.pfalse
-      );
+      Sd_states.Unreachable_Stmts.iter add_assert_false;
       if Sd_options.Behavior_Reachability.get() then
-	Sd_states.Behavior_Reachability.iter (fun _ (kf,bhv,is_reachable) ->
-	  Sd_options.Self.feedback "behavior '%s' of function '%s' %s"
-	    bhv.b_name
-	    (Kernel_function.get_name kf)
-	    (if is_reachable then "reachable" else "not reachable")
-	)
+	Sd_states.Behavior_Reachability.iter info_reachability
     end
 
 
@@ -221,40 +207,37 @@ let setup_props_bijection () =
   List.iter register strengthened_precond
 
 
-let properties_of_behavior : string -> Property.t list =
-  fun name ->
-    Property_status.fold (fun p ret ->
-      match Property.get_behavior p with
-      | Some b when b.b_name = name -> p :: ret
-      | _ -> ret
-    ) []
+let properties_of_behavior name =
+  let gather p ret =
+    match Property.get_behavior p with
+    | Some b when b.b_name = name -> p :: ret
+    | _ -> ret
+  in
+  Property_status.fold gather []
 
 
-let properties_of_function : string -> Property.t list =
-  fun name ->
-    Property_status.fold (fun p ret ->
-      match Property.get_kf p with
-      | Some kf when (Kernel_function.get_name kf) = name -> p :: ret
-      | _ -> ret
-    ) []
+let properties_of_function name =
+  let gather p ret =
+    match Property.get_kf p with
+    | Some kf when (Kernel_function.get_name kf) = name -> p :: ret
+    | _ -> ret
+  in
+  Property_status.fold gather []
 
 
-let properties_of_name : string -> Property.t list =
-  fun name ->
-    Property_status.fold (fun p ret ->
-      let str =
-	try
-	  let buf = Buffer.create 32 in
-	  let fmt = Format.formatter_of_buffer buf in
-	  Property.short_pretty fmt p;
-	  Format.pp_print_flush fmt ();
-	  let str = Buffer.contents buf in
-	  Buffer.clear buf;
-	  str
-	with _ -> ""
-      in
-      if str <> "" && str = name then p :: ret else ret
-    ) []
+let properties_of_name name =
+  let gather p ret =
+    try
+      let buf = Buffer.create 32 in
+      let fmt = Format.formatter_of_buffer buf in
+      Property.short_pretty fmt p;
+      Format.pp_print_flush fmt ();
+      let str = Buffer.contents buf in
+      Buffer.clear buf;
+      if str = name then p :: ret else ret
+    with _ -> ret
+  in
+  Property_status.fold gather []
 
 
 let run() =
@@ -264,46 +247,31 @@ let run() =
       let properties = Sd_options.Properties.get () in
       let behaviors = Sd_options.Behaviors.get () in
       let functions = Sd_options.Functions.get () in
-
       let gather p b = List.rev_append (properties_of_behavior b) p in
       let props = List.fold_left gather [] behaviors in
       let gather p f = List.rev_append (properties_of_function f) p in
       let props = List.fold_left gather props functions in
       let gather p n = List.rev_append (properties_of_name n) p in
       let props = List.fold_left gather props properties in
-      let props =
-	if props = [] then
-	  Property_status.fold (fun p l -> p :: l) []
-	else
-	  props
-      in
-
-      let props = List.filter (fun p ->
+      let app p l = p :: l in
+      let props = if props = [] then Property_status.fold app [] else props in
+      let to_do p =
 	match Property_status.get p with
 	| Property_status.Inconsistent _
-	| Property_status.Best (Property_status.True,_)
-	| Property_status.Best (Property_status.False_if_reachable,_)
-	| Property_status.Best (Property_status.False_and_reachable,_) -> false
 	| Property_status.Never_tried
+	| Property_status.Best (Property_status.False_if_reachable,_)
 	| Property_status.Best (Property_status.Dont_know,_) -> true
-      ) props in
-
-
-      Sd_options.Self.debug
-	~dkey:Sd_options.dkey_properties "selected properties:";
-      List.iter (fun p ->
-	try
-	  let id = Sd_utils.to_id p in
-	  Sd_options.Self.debug ~dkey:Sd_options.dkey_properties
-	    "%a (%i) found" Property.pretty p id
-	with _ -> Sd_options.Self.debug ~dkey:Sd_options.dkey_properties
-	  "%a not found" Property.pretty p
-      ) props;
-
+	| _ -> false
+      in
+      let props = List.filter to_do props in
+      let dkey = Sd_options.dkey_properties in
+      Sd_options.Self.debug ~dkey "selected properties:";
+      let info_prop p =
+	Sd_options.Self.debug ~dkey "%a %sfound" Property.pretty p
+	  (try ignore(Sd_utils.to_id p); "" with Not_found -> "not")
+      in
+      List.iter info_prop props;
       compute_props props;
-
-
-      (* cleaning *)
       Sd_states.Id_To_Property.clear();
       Sd_states.Property_To_Id.clear();
       Sd_states.Not_Translated_Predicates.clear();
