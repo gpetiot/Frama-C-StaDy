14e11f40e3db263b15d2bce018ff81f1eca45c3f
Guillaume Petiot <guillaume.petiot@cea.fr>
2014-11-19 18:27:20 +0100

    oracles updated

--- gmp.c
+++ gmp.c
@@ -1,3 +1,8 @@
-#include<gmp.h>
 
-mpz_t x;
+struct __anonstruct___mpz_struct_1 {\
+   int _mp_alloc ;\
+   int _mp_size ;\
+   unsigned long *_mp_d ;\
+};\
+typedef struct __anonstruct___mpz_struct_1 __mpz_struct;\
+typedef __mpz_struct ( __attribute__((__FC_BUILTIN__)) mpz_t)[1];
