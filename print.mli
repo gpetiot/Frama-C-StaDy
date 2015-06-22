e0481d000c9913ee1ae9ae796f6bdadcec01755b
Guillaume Petiot <guillaume.petiot@cea.fr>
2015-06-22 18:41:22 +0200

    .mli for printer

---
+++ print.mli
@@ -0,0 +1,12 @@
+
+(* Extend the default printer to display the generated code insertions
+ * at the corresponding label and the generated functions. *)
+class print_insertions:
+	(* labeled code insertions *)
+	(Symbolic_label.t, Insertion.t Queue.t) Hashtbl.t ->
+	(* new functions *)
+	Insertions.func list ->
+	(* id of stmts, parameterizing the SWD *)
+	int list ->
+	unit ->
+	Printer.extensible_printer
