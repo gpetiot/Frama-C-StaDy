6ece072d9cf3a252040d03360836b437a7d1e6e4
Guillaume Petiot <g.petiot@mail.com>
2017-03-04 10:17:49 +0100

    function.ml(i) not needed anymore

--- function.mli
+++
@@ -1,7 +0,0 @@
-
-type t
-
-val make: Cil_types.varinfo -> formals:Cil_types.varinfo list ->
-  locals:Cil_types.varinfo list -> Cil_types.stmt list -> t
-val pretty: Format.formatter -> t -> unit
-val is_nondet: t -> bool
